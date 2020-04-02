ca62eb107e18bb0c95e775572a1c5a9b7f6fcbc3
wwwsapir <wwwsapir@gmail.com>
2020-04-02 13:28:48 +0300

    Add monsters characters and images

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const imageBaseUrl = "https://drive.google.com/uc?id=";
+
 const imageLinks = {
-  ranger: "https://drive.google.com/uc?id=1MIyJ4ATQToPI_RN7RfiFQPDmuKBWHz_j",
-  rogue: "https://drive.google.com/uc?id=1bRC8T6ywHD6f23cdLnp2WmWaIImY7YXM",
-  sorcerer: "https://drive.google.com/uc?id=1TK0i8R3td1vj0Xlo1kZmS0Zu7H2c90kK",
-  warlock: "https://drive.google.com/uc?id=1PMrMr1T9WVBa8x5FDqV2ZIOK6Uam_Hw5",
-  fighter: "https://drive.google.com/uc?id=1HJxgNpcvnkyH2Bx36ynbQgIhmBPK1XfA"
+  ranger: imageBaseUrl + "1MIyJ4ATQToPI_RN7RfiFQPDmuKBWHz_j",
+  rogue: imageBaseUrl + "1bRC8T6ywHD6f23cdLnp2WmWaIImY7YXM",
+  sorcerer: imageBaseUrl + "1TK0i8R3td1vj0Xlo1kZmS0Zu7H2c90kK",
+  warlock: imageBaseUrl + "1PMrMr1T9WVBa8x5FDqV2ZIOK6Uam_Hw5",
+  fighter: imageBaseUrl + "1HJxgNpcvnkyH2Bx36ynbQgIhmBPK1XfA",
+  scorbat: imageBaseUrl + "1R4CXRmvvWFQwSWH5blj71R9si4q_ePHb",
+  dragon: imageBaseUrl + "13hzS-a0ZeHNe9f1lkWpfTVrj3prDAs0U",
+  beetle: imageBaseUrl + "1Iy7IJvd4PT7ImBPjSJRrf7Ytc7ZYiOgs",
+  balrog: imageBaseUrl + "1-ShPk6rbliyWzEd7yEFqrJoOZwn1awAM",
 };
 
 ReactDOM.render(
