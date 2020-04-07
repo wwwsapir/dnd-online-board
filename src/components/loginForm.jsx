0e55c97d391b0dcf9bec4b1aea3cd1a7f97e569e
wwwsapir <wwwsapir@gmail.com>
2020-04-07 12:03:21 +0300

    Login and register are now forms and submit on Enter key

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,7 +9,8 @@ class LoginForm extends Component {
     errorMessage: "",
   };
 
-  handleLoginButtonClick = () => {
+  handleLoginFormSubmit = (e) => {
+    e.preventDefault();
     const { email, password } = this.state;
     const { onLogin } = this.props;
     const promise = CallLoginAPI({ email, password });
@@ -28,60 +29,62 @@ class LoginForm extends Component {
     const { email, password, errorMessage } = this.state;
     const { onForgotPassword, onRegistration } = this.props;
     return (
-      <ul
-        className="nav nav-tabs flex-column text-white bg-dark row w-100"
-        style={{ border: "8px double blue", fontSize: 15, padding: 20 }}
-      >
-        <h4 className="col mb-5">
-          <span className="creatorHeader">Welcome to D&amp;D Online Map</span>
-        </h4>
-        <li className="nav-item">
-          <input
-            className="input-group-sm form-control col"
-            id="email"
-            value={email}
-            placeholder="Email Address"
-            required
-            onChange={(event) => this.setState({ email: event.target.value })}
-          />
-        </li>
-        <li className="nav-item">
-          <input
-            className="input-group-sm form-control col mt-3"
-            type="password"
-            id="password"
-            value={password}
-            placeholder="Password"
-            required
-            onChange={(event) =>
-              this.setState({ password: event.target.value })
-            }
-          />
-        </li>
-        <li className="nav-item">
-          <button
-            onClick={this.handleLoginButtonClick}
-            className="btn btn-primary form-control mt-3 col"
-          >
-            Login
-          </button>
-        </li>
-        <li className="nav-item mt-2">
-          <a className="float-right" href="#" onClick={onForgotPassword}>
-            Forgot password?
-          </a>
-          <a href="#" onClick={onRegistration}>
-            New user?
-          </a>
-        </li>
-        {errorMessage ? (
-          <li className="nav-item col">
-            <h4>
-              <span className="badge badge-danger">{errorMessage}</span>
-            </h4>
+      <form onSubmit={this.handleLoginFormSubmit}>
+        <ul
+          className="nav nav-tabs flex-column text-white bg-dark row w-100"
+          style={{ border: "8px double blue", fontSize: 15, padding: 20 }}
+        >
+          <h4 className="col mb-5">
+            <span className="creatorHeader">Welcome to D&amp;D Online Map</span>
+          </h4>
+          <li className="nav-item">
+            <input
+              className="input-group-sm form-control col"
+              id="email"
+              value={email}
+              placeholder="Email Address"
+              required
+              onChange={(event) => this.setState({ email: event.target.value })}
+            />
           </li>
-        ) : null}
-      </ul>
+          <li className="nav-item">
+            <input
+              className="input-group-sm form-control col mt-3"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              required
+              onChange={(event) =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </li>
+          <li className="nav-item">
+            <button
+              type="submit"
+              className="btn btn-primary form-control mt-3 col"
+            >
+              Login
+            </button>
+          </li>
+          <li className="nav-item mt-2">
+            <a className="float-right" href="#" onClick={onForgotPassword}>
+              Forgot password?
+            </a>
+            <a href="#" onClick={onRegistration}>
+              New user?
+            </a>
+          </li>
+          {errorMessage ? (
+            <li className="nav-item col">
+              <h4>
+                <span className="badge badge-danger">{errorMessage}</span>
+              </h4>
+            </li>
+          ) : null}
+        </ul>
+      </form>
     );
   }
 }
--- src/components/registrationForm.jsx
+++ src/components/registrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CallSaveNewGameDataAPI } from "../constants";
+import { CallRegisterAPI } from "../constants";
 
 class RegistrationForm extends Component {
   state = {
@@ -10,17 +10,18 @@ class RegistrationForm extends Component {
     errorMessage: "",
   };
 
-  handleRegisterButtonClick = () => {
+  handleRegisterFormSubmit = (e) => {
+    e.preventDefault();
     const { userName, email, password } = this.state;
     const { onRegistered } = this.props;
-    const promise = CallSaveNewGameDataAPI({ userName, email, password });
+    const promise = CallRegisterAPI({ userName, email, password });
 
     promise.then((res) => {
       if (!res) return;
       if (res.error) {
         this.setState({ errorMessage: res.error.message });
       } else {
-        onRegistered(res.userName, res.authToken);
+        onRegistered();
       }
     });
   };
@@ -29,69 +30,71 @@ class RegistrationForm extends Component {
     const { userName, email, password, errorMessage } = this.state;
     const { onBackToLoginPage } = this.props;
     return (
-      <ul
-        className="nav nav-tabs flex-column text-white bg-dark row w-100"
-        style={{ border: "8px double blue", fontSize: 15, padding: 20 }}
-      >
-        <h4 className="col mb-5">
-          <span className="creatorHeader">New User Registration</span>
-        </h4>
-        <li className="nav-item">
-          <input
-            className="input-group-sm form-control col"
-            id="userName"
-            value={userName}
-            placeholder="User Name"
-            required
-            onChange={(event) =>
-              this.setState({ userName: event.target.value })
-            }
-          />
-        </li>
-        <li className="nav-item">
-          <input
-            className="input-group-sm form-control col mt-3"
-            id="email"
-            value={email}
-            placeholder="Email Address"
-            required
-            onChange={(event) => this.setState({ email: event.target.value })}
-          />
-        </li>
-        <li className="nav-item">
-          <input
-            className="input-group-sm form-control col mt-3"
-            type="password"
-            id="password"
-            value={password}
-            placeholder="Password"
-            required
-            onChange={(event) =>
-              this.setState({ password: event.target.value })
-            }
-          />
-        </li>
-        <li className="nav-item">
-          <button
-            onClick={this.handleRegisterButtonClick}
-            className="btn btn-primary form-control mt-3 col"
-          >
-            Register
-          </button>
-        </li>
-        <li className="nav-item mt-2">
-          <a className="float-right" href="#" onClick={onBackToLoginPage}>
-            Cancel and back to login
-          </a>
-        </li>
-        {errorMessage ? (
-          <li className="nav-item col">
-            <h4>
-              <span className="badge badge-danger">{errorMessage}</span>
-            </h4>
+      <form onSubmit={this.handleRegisterFormSubmit}>
+        <ul
+          className="nav nav-tabs flex-column text-white bg-dark row w-100"
+          style={{ border: "8px double blue", fontSize: 15, padding: 20 }}
+        >
+          <h4 className="col mb-5">
+            <span className="creatorHeader">New User Registration</span>
+          </h4>
+          <li className="nav-item">
+            <input
+              className="input-group-sm form-control col"
+              id="userName"
+              value={userName}
+              placeholder="User Name"
+              required
+              onChange={(event) =>
+                this.setState({ userName: event.target.value })
+              }
+            />
           </li>
-        ) : null}
-      </ul>
+          <li className="nav-item">
+            <input
+              className="input-group-sm form-control col mt-3"
+              id="email"
+              value={email}
+              placeholder="Email Address"
+              required
+              onChange={(event) => this.setState({ email: event.target.value })}
+            />
+          </li>
+          <li className="nav-item">
+            <input
+              className="input-group-sm form-control col mt-3"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              required
+              onChange={(event) =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </li>
+          <li className="nav-item">
+            <button
+              type="submit"
+              className="btn btn-primary form-control mt-3 col"
+            >
+              Register
+            </button>
+          </li>
+          <li className="nav-item mt-2">
+            <a className="float-right" href="#" onClick={onBackToLoginPage}>
+              Cancel and back to login
+            </a>
+          </li>
+          {errorMessage ? (
+            <li className="nav-item col">
+              <h4>
+                <span className="badge badge-danger">{errorMessage}</span>
+              </h4>
+            </li>
+          ) : null}
+        </ul>
+      </form>
     );
   }
 }
