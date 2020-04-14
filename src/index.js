e39c57ae2c515736745bdeb0085d334ff89f7c05
wwwsapir <wwwsapir@gmail.com>
2020-04-14 17:10:54 +0300

    Added a message when user enters from a mobile device

--- src/index.js
+++ src/index.js
@@ -6,21 +6,29 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter, Route } from "react-router-dom";
 import { DEFAULT_MAP_IMG_PATH } from "./constants";
+import { isMobile } from "react-device-detect";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="container-fluid DNDMapContainer">
-      <HashRouter>
-        <Route path="/">
-          <App
-            cellSize={45}
-            rowCount={15}
-            colCount={15}
-            bgImageLink={DEFAULT_MAP_IMG_PATH}
-          />
-        </Route>
-      </HashRouter>
-    </div>
+    {isMobile ? (
+      <div>
+        This app is unavailable on mobile devices. Please use a a desktop to
+        enter :)
+      </div>
+    ) : (
+      <div className="container-fluid DNDMapContainer">
+        <HashRouter>
+          <Route path="/">
+            <App
+              cellSize={45}
+              rowCount={15}
+              colCount={15}
+              bgImageLink={DEFAULT_MAP_IMG_PATH}
+            />
+          </Route>
+        </HashRouter>
+      </div>
+    )}
   </React.StrictMode>,
   document.getElementById("root")
 );
