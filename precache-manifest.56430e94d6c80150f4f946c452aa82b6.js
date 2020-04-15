0626b40696ad9028e700afc642aa2ef705cc860c
wwwsapir <wwwsapir@gmail.com>
2020-04-15 14:05:02 +0300

    Updates

--- precache-manifest.56430e94d6c80150f4f946c452aa82b6.js
+++ precache-manifest.56430e94d6c80150f4f946c452aa82b6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fddd037f3cfeba991b5ad20684cbfeed",
+    "revision": "0fcb866db8609586413d50f728c610b7",
     "url": "/dnd-online-board/index.html"
   },
   {
-    "revision": "9c1274814342a695333c",
+    "revision": "6994bebd6c17bb07488d",
     "url": "/dnd-online-board/static/css/2.86f50a85.chunk.css"
   },
   {
-    "revision": "e34b65287f0198458040",
+    "revision": "2f68dfa97d63f101ed96",
     "url": "/dnd-online-board/static/css/main.b1734917.chunk.css"
   },
   {
-    "revision": "9c1274814342a695333c",
-    "url": "/dnd-online-board/static/js/2.5821d9c7.chunk.js"
+    "revision": "6994bebd6c17bb07488d",
+    "url": "/dnd-online-board/static/js/2.b9993cac.chunk.js"
   },
   {
     "revision": "7acc26b931cb41fd8460a497a13c1ac8",
-    "url": "/dnd-online-board/static/js/2.5821d9c7.chunk.js.LICENSE.txt"
+    "url": "/dnd-online-board/static/js/2.b9993cac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e34b65287f0198458040",
-    "url": "/dnd-online-board/static/js/main.88367d4b.chunk.js"
+    "revision": "2f68dfa97d63f101ed96",
+    "url": "/dnd-online-board/static/js/main.813976a0.chunk.js"
   },
   {
     "revision": "c81961047f69adb36443",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dnd-online-board/precache-manifest.0e5042dca3bdada3ea1d591501bb90e0.js"
+  "/dnd-online-board/precache-manifest.56430e94d6c80150f4f946c452aa82b6.js"
 );
 
 self.addEventListener('message', (event) => {
